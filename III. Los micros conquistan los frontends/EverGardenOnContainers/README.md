16d03ddd83ac2ff7f213d2c3b84abb22305ae94a
Daniel Puig Conca <daniel.puig.conca@everis.com>
2019-06-26 19:01:38 +0200

    EverGardenOnContainers

---
+++ III. Los micros conquistan los frontends/EverGardenOnContainers/README.md
@@ -0,0 +1,13 @@
+# Lerna
+
+## Instalar todas las dependencias
+
+```bash
+lerna exec -- npm i
+```
+
+# Docker
+
+```bash
+docker-compose up
+```
---
+++ III. Los micros conquistan los frontends/EverGardenOnContainers/packages/team-marketing/front/suggestions.js
@@ -0,0 +1,93 @@
+/* eslint-disable no-use-before-define, no-console, class-methods-use-this */
+(function suggestions() {
+
+  class Suggestions extends HTMLElement {
+
+    connectedCallback() {
+      const product_id = this.getAttribute('product_id');
+
+      window.dispatchEvent(new CustomEvent('suggestions:get', {
+        detail: {
+          product_id: product_id
+        }
+      }));
+      console.log('dispatchEvent:suggestions:get');
+
+      window.addEventListener('suggestions:change', (ev) => {
+        this.render(ev.detail.suggestions);
+        console.log('dispatchEvent:suggestions:change');
+      });
+
+
+    }
+
+    select(product) {
+
+      // se informa de un producto seleccionado
+      window.dispatchEvent(new CustomEvent('suggestions:select', {
+        detail: {
+          product: product
+        }
+      }));
+      console.log('dispatchEvent:suggestions:select');
+
+      window.dispatchEvent(new CustomEvent('suggestions:get', {
+        detail: {
+          product_id: product.id
+        }
+      }));
+      console.log('dispatchEvent:suggestions:get');
+
+    }
+
+    addListener(suggestions) {
+      suggestions.forEach(p => {
+        const $element = document.getElementById(`suggestion-${p.id}`);
+        if ($element) $element.addEventListener('click', this.select.bind(this, p));
+      });
+    }
+
+    gettemplateproduct(p) {
+      return `    
+        <figure class="itemside">
+            <div class="aside"><img src="/common/images/items/${p.id}.jpg" class="img-sm"></div>
+            <figcaption class="text-wrap align-self-center">
+                <h6 class="title"><a id='suggestion-${p.id}' href='#' >${p.name}</a></h6>
+                <div class="price-wrap">
+                    <span class="price-new">${p.priceNew}$</span>
+                    <del class="price-old text-muted">${p.priceOld}$</del>
+                </div> 
+            </figcaption>
+        </figure>
+        `
+    }
+
+
+    render(suggestions) {
+
+      let elelments = '';
+      suggestions.forEach(p => {
+        elelments += this.gettemplateproduct(p);
+      });
+
+      this.innerHTML = `
+          <div id="code_itemside_img2">
+              <div class="box items-bordered-wrap">
+             ${elelments}  
+              </div> <!-- box.// -->
+          </div>
+          `;
+
+      this.addListener(suggestions);
+
+    }
+
+    disconnectedCallback() {
+      // window.removeEventListener('product:select',this.render());
+    }
+
+  }
+  window.customElements.define('wc-suggestions', Suggestions);
+
+
+}());
